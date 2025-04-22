b6cf7f6ea4f3b448facc9464ff27a8122d4005b5
Michellic61 <michellic61@gmail.com>
2025-04-22 14:58:26 +0000

    Atualizado o texto da página inicial do app

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="z-10 max-w-4xl w-full text-center font-mono text-lg">
+        <h1>Welcome to the ConchaYOro App </h1>
+      </div>
+    </main>
+  );
+}
