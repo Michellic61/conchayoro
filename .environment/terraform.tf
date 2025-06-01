592b34a5a77cd4e209f3f643e7f99e55590fc152
Michellic61 <110945999+Michellic61@users.noreply.github.com>
2025-06-01 11:38:20 -0300

    configuração do Terraform e backend remoto

---
+++ .environment/terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.98"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "conchayoro-lfs"                # Substitua pelo nome do seu bucket
+    key     = "state/terraform.tfstate"       # Caminho do arquivo de estado remoto
+    region  = "us-east-1"                     # Região obrigatória para AWS Educate
+    encrypt = true                            # Criptografa o state
+  }
+
+  required_version = ">= 1.12.1"
+}
+
+provider "aws" {
+  region = var.AWS_REGION
+}
