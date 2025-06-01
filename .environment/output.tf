5796ebb088cfd61f7ddcfadc46f9f52839ea46ce
Michellic61 <110945999+Michellic61@users.noreply.github.com>
2025-06-01 12:30:42 -0300

    Update output.tf

--- .environment/output.tf
+++ .environment/output.tf
@@ -1,3 +1,6 @@
+# ------------------------------------------------------------
+# output.tf — saída útil para debug e consulta
+# ------------------------------------------------------------
 output "ecr_repository_name" {
   description = "ECR repository name"
   value       = aws_ecr_repository.cyo_ecr_repo.name
@@ -17,4 +20,3 @@ output "elastic_beanstalk_endpoint_url" {
   description = "elastic_beanstalk_endpoint_url"
   value       = aws_elastic_beanstalk_environment.cyo_ebef.endpoint_url
 }
-
