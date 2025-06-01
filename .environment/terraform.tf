b27760b6129b886cbd8d819ff150756cc57c61f3
Michellic61 <110945999+Michellic61@users.noreply.github.com>
2025-06-01 12:51:42 -0300

    Update terraform.tf

--- .environment/terraform.tf
+++ .environment/terraform.tf
@@ -1,6 +1,3 @@
-# ------------------------------------------------------------
-# terraform.tf — configuração do Terraform e backend remoto
-# ------------------------------------------------------------
 terraform {
   required_providers {
     aws = {
@@ -9,12 +6,7 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket  = "conchayoro-mmc"                # Substitua pelo nome do seu bucket
-    key     = "state/terraform.tfstate"       # Caminho do arquivo de estado remoto
-    region  = "us-east-1"                     # Região obrigatória para AWS Educate
-    encrypt = true                            # Criptografa o state
-  }
+  backend "s3" {}
 
   required_version = ">= 1.12.1"
 }
